ca43b3b61626e700d8ba4dfbbaa1187a56c4982c
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2024-08-03 13:43:05 +0300

    Extracting information

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -1,20 +1,33 @@
-#import pandas as pd
-from bs4 import BeautifulSoup as bs
-from requests import get
+import pandas as pd
+import numpy as np
+import sqlite3
+import datetime
+from bs4 import BeautifulSoup
+import requests
+# Code for ETL operations on Country-GDP data
+def extract(url, table_attribs):
+    url ='https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
 
-page = get('https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29')
-
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-countries_table = soup.find_all(//*[@id="mw-content-text"]/div[1]/table[2]/tbody)
-link=[]
-for row in countries_table:
-    hrefs= row.find_all('a', href=True)
-    for href in hrefs:
-        link.append(href['href'])
-
-print(link)
+def transform(df):
+    GDP_list=df["GDP_USD_millions"].tolist()
+    GDP_list= [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list= [np.round(x/1000,2) for x in GDP_list]
+    
+  
 
 
 
